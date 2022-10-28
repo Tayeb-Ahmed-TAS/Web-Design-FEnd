e4f08ae7bd6cbf1f23a6493e8bae61a07a37c23c
Tayeb Ahmed <104755858+Tayeb-Ahmed-TAS@users.noreply.github.com>
2022-10-28 08:37:22 +0600

    Delete Phase 1 directory

--- Phase 1/js/aboutMe.js
+++
@@ -1,7 +0,0 @@
-// todo: For setting Colon (:) ;
-
-let cv_li_colon = document.querySelectorAll(".cv_left ul li span");
-
-cv_li_colon.forEach((e) => {
-  e.innerHTML = " : ";
-});
--- Phase 1/js/loader.js
+++
@@ -1,7 +0,0 @@
-// todo: Preloader ;
-
-let loader = document.querySelector(".loaderWrapper");
-
-window.addEventListener("load", () => {
-  loader.style.display = "none";
-});
