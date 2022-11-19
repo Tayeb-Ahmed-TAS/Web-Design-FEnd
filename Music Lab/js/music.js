56ae876284bcc6d35188f30267a82df3f1e0dd32
Tayeb Ahmed <104755858+Tayeb-Ahmed-TAS@users.noreply.github.com>
2022-11-19 14:44:44 +0600

    Add files via upload

---
+++ Music Lab/js/music.js
@@ -0,0 +1,14 @@
+let music_icon = document.querySelector(".music img"),
+  audio = document.querySelector(".music audio");
+
+music_icon.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+
+    music_icon.src = "../img/pause.png";
+  } else {
+    audio.pause();
+
+    music_icon.src = "../img/play.png";
+  }
+});
