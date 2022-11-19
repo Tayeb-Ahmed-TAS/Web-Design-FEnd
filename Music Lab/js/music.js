5003d7912088c3e6c54c0bb987803d6749428529
Tayeb Ahmed <104755858+Tayeb-Ahmed-TAS@users.noreply.github.com>
2022-11-19 14:47:56 +0600

    Update music.js

--- Music Lab/js/music.js
+++ Music Lab/js/music.js
@@ -5,10 +5,10 @@ music_icon.addEventListener("click", () => {
   if (audio.paused) {
     audio.play();
 
-    music_icon.src = "../img/pause.png";
+    music_icon.src = "img/pause.png";
   } else {
     audio.pause();
 
-    music_icon.src = "../img/play.png";
+    music_icon.src = "img/play.png";
   }
 });
